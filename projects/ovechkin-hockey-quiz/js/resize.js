6e2304de7181c999ed8637113c06f1c9fd1d4650
Vsevolod Kochnev <v.kochnev@sports.ru>
2025-04-17 11:08:26 +0300

    update resize

--- projects/ovechkin-hockey-quiz/js/resize.js
+++ projects/ovechkin-hockey-quiz/js/resize.js
@@ -1,19 +1,19 @@
 const projectSlug = 'ovechkin-hockey-quiz';
 
-// const resize = () => {
-//     const dataUTILS = {
-//         for: 'BASIC_TEST',
-//         action: 'resizeIframe',
-//         selector: `iframe[src*=\\/projects\\/${projectSlug}]`,
-//         sizes: {
-//             height: 2 * Math.floor(document.body.scrollHeight / 2) + 10,
-//         },
-//     };
+const resize = () => {
+    const dataUTILS = {
+        for: 'BASIC_TEST',
+        action: 'resizeIframe',
+        selector: `iframe[src*=\\/projects\\/${projectSlug}]`,
+        sizes: {
+            height: 2 * Math.floor(document.body.scrollHeight / 2) + 10,
+        },
+    };
 
-//     window?.top?.postMessage(JSON.stringify(dataUTILS), '*');
-// }
+    window?.top?.postMessage(JSON.stringify(dataUTILS), '*');
+}
 
-// const resizeObserver = new ResizeObserver(() => {
-//     resize();
-// });
-// resizeObserver.observe(document.body); 
+const resizeObserver = new ResizeObserver(() => {
+    resize();
+});
+resizeObserver.observe(document.body); 
