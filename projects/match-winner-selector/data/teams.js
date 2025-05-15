fe4755912dd43769944c13a34be8255ff8caafd8
eduardtulipe1995 <eduardred1995@gmail.com>
2025-05-15 16:46:34 +0300

    Создан виджет 'Кто победит?' для матча Спартак - Ростов

---
+++ projects/match-winner-selector/data/teams.js
@@ -0,0 +1,12 @@
+const teamsData = {
+    spartak: {
+        name: "Спартак",
+        votes: 345, // Начальное число голосов за Спартак
+        logo: "./data/spartak-logo.svg"
+    },
+    rostov: {
+        name: "Ростов",
+        votes: 284, // Начальное число голосов за Ростов
+        logo: "./data/rostov-logo.svg" 
+    }
+}; 
