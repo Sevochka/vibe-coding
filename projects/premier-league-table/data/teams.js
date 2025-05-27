945b882905d500a4117bd1e867b17e32e2d1428e
eduardtulipe1995 <eduardred1995@gmail.com>
2025-05-27 18:52:16 +0300

    Обновлены логотипы команд и расширены результаты для полного сезона АПЛ

--- projects/premier-league-table/data/teams.js
+++ projects/premier-league-table/data/teams.js
@@ -3,120 +3,120 @@ const teams = [
         id: 'ARS',
         name: 'Арсенал',
         shortName: 'ARS',
-        logo: 'https://img.championat.com/team/logo/16469924321925886307.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Arsenal-FC-logo.png'
     },
     {
         id: 'AVL',
         name: 'Астон Вилла',
         shortName: 'AVL',
-        logo: 'https://img.championat.com/team/logo/16469924131349605114.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Aston-Villa-FC-logo.png'
     },
     {
         id: 'BOU',
         name: 'Борнмут',
         shortName: 'BOU',
-        logo: 'https://img.championat.com/team/logo/1646992435178969285.png'
+        logo: 'https://loodibee.com/wp-content/uploads/AFC-Bournemouth-logo.png'
     },
     {
         id: 'BRE',
         name: 'Брентфорд',
         shortName: 'BRE',
-        logo: 'https://img.championat.com/team/logo/1644409650271459714.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Brentford-FC-logo.png'
     },
     {
         id: 'BHA',
         name: 'Брайтон',
         shortName: 'BHA',
-        logo: 'https://img.championat.com/team/logo/1646992442889694933.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Brighton-Hove-Albion-logo.png'
     },
     {
         id: 'CHE',
         name: 'Челси',
         shortName: 'CHE',
-        logo: 'https://img.championat.com/team/logo/16469924501949518334.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Chelsea-FC-logo.png'
     },
     {
         id: 'CRY',
         name: 'Кристал Пэлас',
         shortName: 'CRY',
-        logo: 'https://img.championat.com/team/logo/16469924481747347520.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Crystal-Palace-FC-logo.png'
     },
     {
         id: 'EVE',
         name: 'Эвертон',
         shortName: 'EVE',
-        logo: 'https://img.championat.com/team/logo/16469924531566217752.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Everton-FC-logo.png'
     },
     {
         id: 'FUL',
         name: 'Фулхэм',
         shortName: 'FUL',
-        logo: 'https://img.championat.com/team/logo/16469924731384057602.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Fulham-FC-logo.png'
     },
     {
         id: 'IPW',
         name: 'Ипсвич',
         shortName: 'IPW',
-        logo: 'https://img.championat.com/team/logo/1649934694903599223.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Ipswich-Town-FC-logo.png'
     },
     {
         id: 'LEI',
         name: 'Лестер',
         shortName: 'LEI',
-        logo: 'https://img.championat.com/team/logo/1649934694903599223.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Leicester-City-FC-logo.png'
     },
     {
         id: 'LIV',
         name: 'Ливерпуль',
         shortName: 'LIV',
-        logo: 'https://img.championat.com/team/logo/16469924591613339078.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Liverpool-FC-logo.png'
     },
     {
         id: 'MCI',
         name: 'Манчестер Сити',
         shortName: 'MCI',
-        logo: 'https://img.championat.com/team/logo/16469924611780333657.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Manchester-City-FC-logo.png'
     },
     {
         id: 'MUN',
         name: 'Манчестер Юнайтед',
         shortName: 'MUN',
-        logo: 'https://img.championat.com/team/logo/16469924631494456324.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Manchester-United-FC-logo.png'
     },
     {
         id: 'NEW',
         name: 'Ньюкасл',
         shortName: 'NEW',
-        logo: 'https://img.championat.com/team/logo/1646992466908594982.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Newcastle-United-logo.png'
     },
     {
         id: 'NFO',
         name: 'Ноттингем Форест',
         shortName: 'NFO',
-        logo: 'https://img.championat.com/team/logo/16469924691307984933.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Nottingham-Forest-FC-logo.png'
     },
     {
         id: 'SOU',
         name: 'Саутгемптон',
         shortName: 'SOU',
-        logo: 'https://img.championat.com/team/logo/16469924761654878633.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Southampton-FC-logo.png'
     },
     {
         id: 'TOT',
         name: 'Тоттенхэм',
         shortName: 'TOT',
-        logo: 'https://img.championat.com/team/logo/16469924791695775695.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Tottenham-Hotspur-logo.png'
     },
     {
         id: 'WHU',
         name: 'Вест Хэм',
         shortName: 'WHU',
-        logo: 'https://img.championat.com/team/logo/1646992446211602650.png'
+        logo: 'https://loodibee.com/wp-content/uploads/West-Ham-United-FC-logo.png'
     },
     {
         id: 'WOL',
         name: 'Вулверхэмптон',
         shortName: 'WOL',
-        logo: 'https://img.championat.com/team/logo/16469924821969649417.png'
+        logo: 'https://loodibee.com/wp-content/uploads/Wolverhampton-Wanderers-logo.png'
     }
 ]; 
