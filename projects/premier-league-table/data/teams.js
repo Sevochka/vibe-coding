793f88373b3b3832fffd4fcaff80e00640e18459
eduardtulipe1995 <eduardred1995@gmail.com>
2025-05-28 01:11:25 +0300

    Обновлены ссылки на логотипы команд и добавлен тестовый файл для проверки логотипов

--- projects/premier-league-table/data/teams.js
+++ projects/premier-league-table/data/teams.js
@@ -3,120 +3,120 @@ const teams = [
         id: 'ARS',
         name: 'Арсенал',
         shortName: 'ARS',
-        logo: 'https://cdn.sports.ru/images/teams/1/113.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'AVL',
         name: 'Астон Вилла',
         shortName: 'AVL',
-        logo: 'https://cdn.sports.ru/images/teams/1/114.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'BOU',
         name: 'Борнмут',
         shortName: 'BOU',
-        logo: 'https://cdn.sports.ru/images/teams/1/224.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'BRE',
         name: 'Брентфорд',
         shortName: 'BRE',
-        logo: 'https://cdn.sports.ru/images/teams/1/1752.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'BHA',
         name: 'Брайтон',
         shortName: 'BHA',
-        logo: 'https://cdn.sports.ru/images/teams/1/122.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'CHE',
         name: 'Челси',
         shortName: 'CHE',
-        logo: 'https://cdn.sports.ru/images/teams/1/119.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'CRY',
         name: 'Кристал Пэлас',
         shortName: 'CRY',
-        logo: 'https://cdn.sports.ru/images/teams/1/123.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'EVE',
         name: 'Эвертон',
         shortName: 'EVE',
-        logo: 'https://cdn.sports.ru/images/teams/1/121.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'FUL',
         name: 'Фулхэм',
         shortName: 'FUL',
-        logo: 'https://cdn.sports.ru/images/teams/1/125.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'IPW',
         name: 'Ипсвич',
         shortName: 'IPW',
-        logo: 'https://cdn.sports.ru/images/teams/1/130.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'LEI',
         name: 'Лестер',
         shortName: 'LEI',
-        logo: 'https://cdn.sports.ru/images/teams/1/136.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'LIV',
         name: 'Ливерпуль',
         shortName: 'LIV',
-        logo: 'https://cdn.sports.ru/images/teams/1/116.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'MCI',
         name: 'Манчестер Сити',
         shortName: 'MCI',
-        logo: 'https://cdn.sports.ru/images/teams/1/117.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'MUN',
         name: 'Манчестер Юнайтед',
         shortName: 'MUN',
-        logo: 'https://cdn.sports.ru/images/teams/1/118.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'NEW',
         name: 'Ньюкасл',
         shortName: 'NEW',
-        logo: 'https://cdn.sports.ru/images/teams/1/129.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'NFO',
         name: 'Ноттингем Форест',
         shortName: 'NFO',
-        logo: 'https://cdn.sports.ru/images/teams/1/142.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'SOU',
         name: 'Саутгемптон',
         shortName: 'SOU',
-        logo: 'https://cdn.sports.ru/images/teams/1/131.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'TOT',
         name: 'Тоттенхэм',
         shortName: 'TOT',
-        logo: 'https://cdn.sports.ru/images/teams/1/120.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'WHU',
         name: 'Вест Хэм',
         shortName: 'WHU',
-        logo: 'https://cdn.sports.ru/images/teams/1/134.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     },
     {
         id: 'WOL',
         name: 'Вулверхэмптон',
         shortName: 'WOL',
-        logo: 'https://cdn.sports.ru/images/teams/1/145.png'
+        logo: 'https://cdn-m.sport24.ru/m/1359/87cd/bd62/c2ad/ef95/5947/53f7/af93/400_10000_max.png'
     }
 ]; 
