75ab53fdfc4a9032ae1133467abe99fbb6a88586
eduardtulipe1995 <eduardred1995@gmail.com>
2025-05-28 01:08:12 +0300

    Обновлены ссылки на логотипы команд с использованием cdn.sports.ru

--- projects/premier-league-table/data/teams.js
+++ projects/premier-league-table/data/teams.js
@@ -3,120 +3,120 @@ const teams = [
         id: 'ARS',
         name: 'Арсенал',
         shortName: 'ARS',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/9/9e/Arsenal_FC.svg/120px-Arsenal_FC.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/113.png'
     },
     {
         id: 'AVL',
         name: 'Астон Вилла',
         shortName: 'AVL',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/0/0c/Aston_Villa_FC.svg/100px-Aston_Villa_FC.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/114.png'
     },
     {
         id: 'BOU',
         name: 'Борнмут',
         shortName: 'BOU',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/e/e5/AFC_Bournemouth_%282013%29.svg/110px-AFC_Bournemouth_%282013%29.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/224.png'
     },
     {
         id: 'BRE',
         name: 'Брентфорд',
         shortName: 'BRE',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/2/2a/Brentford_FC_crest.svg/100px-Brentford_FC_crest.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/1752.png'
     },
     {
         id: 'BHA',
         name: 'Брайтон',
         shortName: 'BHA',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/fd/Brighton_%26_Hove_Albion_logo.svg/110px-Brighton_%26_Hove_Albion_logo.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/122.png'
     },
     {
         id: 'CHE',
         name: 'Челси',
         shortName: 'CHE',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/cc/Chelsea_FC.svg/110px-Chelsea_FC.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/119.png'
     },
     {
         id: 'CRY',
         name: 'Кристал Пэлас',
         shortName: 'CRY',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/0/0c/Crystal_Palace_FC_logo.svg/100px-Crystal_Palace_FC_logo.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/123.png'
     },
     {
         id: 'EVE',
         name: 'Эвертон',
         shortName: 'EVE',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/7c/Everton_FC_logo.svg/100px-Everton_FC_logo.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/121.png'
     },
     {
         id: 'FUL',
         name: 'Фулхэм',
         shortName: 'FUL',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/e/eb/Fulham_FC.svg/100px-Fulham_FC.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/125.png'
     },
     {
         id: 'IPW',
         name: 'Ипсвич',
         shortName: 'IPW',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/2/2b/Ipswich_Town.svg/110px-Ipswich_Town.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/130.png'
     },
     {
         id: 'LEI',
         name: 'Лестер',
         shortName: 'LEI',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/2/2d/Leicester_City_crest.svg/110px-Leicester_City_crest.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/136.png'
     },
     {
         id: 'LIV',
         name: 'Ливерпуль',
         shortName: 'LIV',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/0/0c/Liverpool_FC.svg/110px-Liverpool_FC.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/116.png'
     },
     {
         id: 'MCI',
         name: 'Манчестер Сити',
         shortName: 'MCI',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/e/eb/Manchester_City_FC_badge.svg/110px-Manchester_City_FC_badge.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/117.png'
     },
     {
         id: 'MUN',
         name: 'Манчестер Юнайтед',
         shortName: 'MUN',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/7a/Manchester_United_FC_crest.svg/110px-Manchester_United_FC_crest.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/118.png'
     },
     {
         id: 'NEW',
         name: 'Ньюкасл',
         shortName: 'NEW',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/5/56/Newcastle_United_Logo.svg/110px-Newcastle_United_Logo.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/129.png'
     },
     {
         id: 'NFO',
         name: 'Ноттингем Форест',
         shortName: 'NFO',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/e/e5/Nottingham_Forest_F.C._logo.svg/110px-Nottingham_Forest_F.C._logo.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/142.png'
     },
     {
         id: 'SOU',
         name: 'Саутгемптон',
         shortName: 'SOU',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/c9/FC_Southampton.svg/110px-FC_Southampton.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/131.png'
     },
     {
         id: 'TOT',
         name: 'Тоттенхэм',
         shortName: 'TOT',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/b/b4/Tottenham_Hotspur.svg/110px-Tottenham_Hotspur.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/120.png'
     },
     {
         id: 'WHU',
         name: 'Вест Хэм',
         shortName: 'WHU',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/c2/West_Ham_United_FC_logo.svg/110px-West_Ham_United_FC_logo.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/134.png'
     },
     {
         id: 'WOL',
         name: 'Вулверхэмптон',
         shortName: 'WOL',
-        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/fc/Wolverhampton_Wanderers.svg/110px-Wolverhampton_Wanderers.svg.png'
+        logo: 'https://cdn.sports.ru/images/teams/1/145.png'
     }
 ]; 
