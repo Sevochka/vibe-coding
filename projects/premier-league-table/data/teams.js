88c8f3f903dda17d8488d0db8f9c22bc6d8cce34
eduardtulipe1995 <eduardred1995@gmail.com>
2025-05-28 00:57:36 +0300

    Обновлены ссылки на логотипы команд с использованием Wikimedia

--- projects/premier-league-table/data/teams.js
+++ projects/premier-league-table/data/teams.js
@@ -3,120 +3,120 @@ const teams = [
         id: 'ARS',
         name: 'Арсенал',
         shortName: 'ARS',
-        logo: 'https://loodibee.com/wp-content/uploads/Arsenal-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/9/9e/Arsenal_FC.svg/120px-Arsenal_FC.svg.png'
     },
     {
         id: 'AVL',
         name: 'Астон Вилла',
         shortName: 'AVL',
-        logo: 'https://loodibee.com/wp-content/uploads/Aston-Villa-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/0/0c/Aston_Villa_FC.svg/100px-Aston_Villa_FC.svg.png'
     },
     {
         id: 'BOU',
         name: 'Борнмут',
         shortName: 'BOU',
-        logo: 'https://loodibee.com/wp-content/uploads/AFC-Bournemouth-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/e/e5/AFC_Bournemouth_%282013%29.svg/110px-AFC_Bournemouth_%282013%29.svg.png'
     },
     {
         id: 'BRE',
         name: 'Брентфорд',
         shortName: 'BRE',
-        logo: 'https://loodibee.com/wp-content/uploads/Brentford-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/2/2a/Brentford_FC_crest.svg/100px-Brentford_FC_crest.svg.png'
     },
     {
         id: 'BHA',
         name: 'Брайтон',
         shortName: 'BHA',
-        logo: 'https://loodibee.com/wp-content/uploads/Brighton-Hove-Albion-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/fd/Brighton_%26_Hove_Albion_logo.svg/110px-Brighton_%26_Hove_Albion_logo.svg.png'
     },
     {
         id: 'CHE',
         name: 'Челси',
         shortName: 'CHE',
-        logo: 'https://loodibee.com/wp-content/uploads/Chelsea-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/cc/Chelsea_FC.svg/110px-Chelsea_FC.svg.png'
     },
     {
         id: 'CRY',
         name: 'Кристал Пэлас',
         shortName: 'CRY',
-        logo: 'https://loodibee.com/wp-content/uploads/Crystal-Palace-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/0/0c/Crystal_Palace_FC_logo.svg/100px-Crystal_Palace_FC_logo.svg.png'
     },
     {
         id: 'EVE',
         name: 'Эвертон',
         shortName: 'EVE',
-        logo: 'https://loodibee.com/wp-content/uploads/Everton-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/7c/Everton_FC_logo.svg/100px-Everton_FC_logo.svg.png'
     },
     {
         id: 'FUL',
         name: 'Фулхэм',
         shortName: 'FUL',
-        logo: 'https://loodibee.com/wp-content/uploads/Fulham-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/e/eb/Fulham_FC.svg/100px-Fulham_FC.svg.png'
     },
     {
         id: 'IPW',
         name: 'Ипсвич',
         shortName: 'IPW',
-        logo: 'https://loodibee.com/wp-content/uploads/Ipswich-Town-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/2/2b/Ipswich_Town.svg/110px-Ipswich_Town.svg.png'
     },
     {
         id: 'LEI',
         name: 'Лестер',
         shortName: 'LEI',
-        logo: 'https://loodibee.com/wp-content/uploads/Leicester-City-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/2/2d/Leicester_City_crest.svg/110px-Leicester_City_crest.svg.png'
     },
     {
         id: 'LIV',
         name: 'Ливерпуль',
         shortName: 'LIV',
-        logo: 'https://loodibee.com/wp-content/uploads/Liverpool-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/0/0c/Liverpool_FC.svg/110px-Liverpool_FC.svg.png'
     },
     {
         id: 'MCI',
         name: 'Манчестер Сити',
         shortName: 'MCI',
-        logo: 'https://loodibee.com/wp-content/uploads/Manchester-City-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/e/eb/Manchester_City_FC_badge.svg/110px-Manchester_City_FC_badge.svg.png'
     },
     {
         id: 'MUN',
         name: 'Манчестер Юнайтед',
         shortName: 'MUN',
-        logo: 'https://loodibee.com/wp-content/uploads/Manchester-United-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/7/7a/Manchester_United_FC_crest.svg/110px-Manchester_United_FC_crest.svg.png'
     },
     {
         id: 'NEW',
         name: 'Ньюкасл',
         shortName: 'NEW',
-        logo: 'https://loodibee.com/wp-content/uploads/Newcastle-United-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/5/56/Newcastle_United_Logo.svg/110px-Newcastle_United_Logo.svg.png'
     },
     {
         id: 'NFO',
         name: 'Ноттингем Форест',
         shortName: 'NFO',
-        logo: 'https://loodibee.com/wp-content/uploads/Nottingham-Forest-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/e/e5/Nottingham_Forest_F.C._logo.svg/110px-Nottingham_Forest_F.C._logo.svg.png'
     },
     {
         id: 'SOU',
         name: 'Саутгемптон',
         shortName: 'SOU',
-        logo: 'https://loodibee.com/wp-content/uploads/Southampton-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/c9/FC_Southampton.svg/110px-FC_Southampton.svg.png'
     },
     {
         id: 'TOT',
         name: 'Тоттенхэм',
         shortName: 'TOT',
-        logo: 'https://loodibee.com/wp-content/uploads/Tottenham-Hotspur-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/b/b4/Tottenham_Hotspur.svg/110px-Tottenham_Hotspur.svg.png'
     },
     {
         id: 'WHU',
         name: 'Вест Хэм',
         shortName: 'WHU',
-        logo: 'https://loodibee.com/wp-content/uploads/West-Ham-United-FC-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/c/c2/West_Ham_United_FC_logo.svg/110px-West_Ham_United_FC_logo.svg.png'
     },
     {
         id: 'WOL',
         name: 'Вулверхэмптон',
         shortName: 'WOL',
-        logo: 'https://loodibee.com/wp-content/uploads/Wolverhampton-Wanderers-logo.png'
+        logo: 'https://upload.wikimedia.org/wikipedia/ru/thumb/f/fc/Wolverhampton_Wanderers.svg/110px-Wolverhampton_Wanderers.svg.png'
     }
 ]; 
