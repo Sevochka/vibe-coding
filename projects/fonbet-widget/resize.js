1d316b5a42284bac70f613863f0b2be4221926bd
Vsevolod Kochnev <v.kochnev@sports.ru>
2025-06-20 17:53:08 +0300

    fix

--- projects/fonbet-widget/resize.js
+++ projects/fonbet-widget/resize.js
@@ -2,7 +2,7 @@ const resize = () => {
       const dataUTILS = {
         for: 'BASIC_TEST',
         action: 'resizeIframe',
-        selector: `iframe[src*=\/projects\/fonbet-widget]`,
+        selector: `iframe[src*="/projects/fonbet-widget"]`,
         sizes: {
           height: document.body.scrollHeight,
         },
